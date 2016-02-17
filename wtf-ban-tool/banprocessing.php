58091e86fa119261bc39ad60fe2463789d0f7db0
Roberts Slisans <Roberts Slisans>
2016-02-17 20:58:28 +0000

    Added 1-2-2-1 format and finished first round. Add UI for room creation.

--- wtf-ban-tool/banprocessing.php
+++ wtf-ban-tool/banprocessing.php
@@ -1,16 +1,14 @@
 <?php
 
 
-	session_start();
-mysql_connect("mysql.cba.pl","zychuu97","<well there was my pass xD>") or die("error with connecting to db");
-mysql_select_db("zychuu_cba_pl") or die("error with chosing db");
-	mysql_query ('SET NAMES utf8');
+	require("dbconnect.php");
 
 	
 	$id=$_GET['id'];
 	$map=$_GET['map'];
+	$p=$_GET['p'];
 	
-	if ($map!="ref") {mysql_query("UPDATE sessions SET $map=1 WHERE id='$id' "); }
+	if ($map!="ref") {mysql_query("UPDATE sessions SET $map=1, step=step +1 WHERE id='$id' "); }
 
 	$select=mysql_query("SELECT * FROM sessions where id='$id' ");
 
@@ -24,48 +22,27 @@ mysql_select_db("zychuu_cba_pl") or die("error with chosing db");
 	$stadium=$row['stadium'];
 	$twillight=$row['twillight'];
 	$step=$row['step'];
-	
-		if ($blackguard==0)
-		{
-			echo "<div id=\"blackguard\"><img src=\"pic/blackguard.jpg\" onclick='dostuffandthings(\"blackguard\")'></div>" ;
-		}
-		else {echo "<div id=\"blackguard\"><img src=\"pic/blackguardban.jpg\"></div>";}
-		
-		if ($kings==0)
-		{
-			echo "<div id=\"kings\"><img src=\"pic/kings.jpg\" onclick='dostuffandthings(\"kings\")'></div>" ;
-		}
-		else {echo "<div id=\"kings\"><img src=\"pic/kingsban.jpg\"></div>";}
-		
-		if ($mammoth==0)
-		{
-			echo "<div id=\"mammoth\"><img src=\"pic/mammoth.jpg\" onclick='dostuffandthings(\"mammoth\")'></div>" ;
-		}
-		else {echo "<div id=\"mammoth\"><img src=\"pic/mammothban.jpg\"></div>";}
-		
-		if ($shipwreck==0)
-		{
-			echo "<div id=\"shipwreck\"><img src=\"pic/shipwreck.jpg\" onclick='dostuffandthings(\"shipwreck\")'></div>" ;
-		}
-		else {echo "<div id=\"shipwreck\"><img src=\"pic/shipwreckban.jpg\"></div>";}
-		
-		if ($shithall==0)
-		{
-			echo "<div id=\"shithall\"><img src=\"pic/shithall.jpg\" onclick='dostuffandthings(\"shithall\")'></div>" ;
-		}
-		else {echo "<div id=\"shithall\"><img src=\"pic/shithallban.jpg\"></div>";}
-		
-		if ($stadium==0)
-		{
-			echo "<div id=\"stadium\"><img src=\"pic/stadium.jpg\" onclick='dostuffandthings(\"stadium\")'></div>" ;
-		}
-		else {echo "<div id=\"stadium\"><img src=\"pic/stadiumban.jpg\"></div>";}
-		
-		if ($twillight==0)
-		{
-			echo "<div id=\"twillight\"><img src=\"pic/twillight.jpg\" onclick='dostuffandthings(\"twillight\")'></div>" ;
-		}
-		else {echo "<div id=\"twillight\"><img src=\"pic/twillightban.jpg\"></div>";}
-		
 	}
+	
+	if ( ($step==1 OR $step==4 OR $step==5)  AND $p==1)
+	    {
+			include("maps-buttons.php");
+		}
+	
+	elseif ( ($step==2 OR $step==3 OR $step==6)  AND $p==2)
+	{
+		include("maps-buttons.php");
+	}
+	
+	elseif ($step==7)
+	{
+		include("maps-spect-buttons.php");
+	}
+	
+	else
+		
+		{
+			include("maps-spect-buttons.php");
+		}
+	
 ?>
--- wtf-ban-tool/starter.php
+++ wtf-ban-tool/starter.php
@@ -1,16 +1,16 @@
 <?php
 
 
-	session_start();
-mysql_connect("mysql.cba.pl","zychuu97","do64k87y") or die("blad laczenia z baza");
-mysql_select_db("zychuu_cba_pl") or die("blad z wybieraniem bazy");
-	mysql_query ('SET NAMES utf8');
-	$id=session_id();
+require("dbconnect.php");
+	
+	$id=$_POST['id'];
+	$p=$_POST['p'];
 	$query="INSERT INTO sessions VALUES ('$id','0','0','0','0','0','0','0','1') ";
 	mysql_query($query);
 
-echo "<a href='bantool.php?id=$id&p=1'> LINK </a> <br>";
-echo "Share with opp: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=2";
+if ($p==1){echo "Link for you: <a href='bantool.php?id=$id&p=1'> LINK </a> <br>
+				Share with opp: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=2 <br>"; }
+echo "Spect link: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=3";
 
 session_unset();	
 session_destroy();
