eb0d75d87230da64261e70f393631d13f27c9d19
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-17 23:26:16 +0000

    Fix formatting, again x2.

--- ban-tool/banprocessing.php
+++ ban-tool/banprocessing.php
@@ -2,12 +2,13 @@
 
 require("dbconnect.php");
 
-
 $id = $_GET['id'];
 $map = $_GET['map'];
 $p = $_GET['p'];
 
-if ($map != "ref") { mysql_query("UPDATE sessions SET $map=1, step=step +1 WHERE id='$id' "); }
+if ($map != "ref") {
+    mysql_query("UPDATE sessions SET $map=1, step=step +1 WHERE id='$id' ");
+}
 
 $select = mysql_query("SELECT * FROM sessions where id='$id' ");
 
@@ -30,17 +31,15 @@ if ( ($step == 1 OR $step == 4 OR $step == 5)  AND $p == 1 )
 
 elseif ( ($step == 2 OR $step == 3 OR $step == 6)  AND $p == 2 )
 {
-        include("maps-buttons.php");
+    include("maps-buttons.php");
 }
 
 elseif ($step == 7)
 {
-        include("maps-spect-buttons.php");
+    include("maps-spect-buttons.php");
 }
 
 else
 {
     include("maps-spect-buttons.php");
 }
-
-?>
