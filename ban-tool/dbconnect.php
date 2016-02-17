e9286783a72755cd767d2178070572c74b04f012
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-17 23:56:06 +0000

    Fix formatting.

--- ban-tool/dbconnect.php
+++ ban-tool/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-	session_start();
+session_start();
 mysql_connect("mysql.cba.pl","zychuu97","<db password here>") or die("error with connecting to db");
 mysql_select_db("zychuu_cba_pl") or die("error with chosing db");
-	mysql_query ('SET NAMES utf8');
+mysql_query ('SET NAMES utf8');
 
 
 ?>
--- ban-tool/starter.php
+++ ban-tool/starter.php
@@ -3,13 +3,15 @@
 
 require("dbconnect.php");
 	
-	$id=$_POST['id'];
-	$p=$_POST['p'];
-	$query="INSERT INTO sessions VALUES ('$id','0','0','0','0','0','0','0','1') ";
-	mysql_query($query);
+$id=$_POST['id'];
+$p=$_POST['p'];
+$query="INSERT INTO sessions VALUES ('$id','0','0','0','0','0','0','0','1') ";
+mysql_query($query);
 
-if ($p==1){echo "Link for you: <a href='bantool.php?id=$id&p=1'> LINK </a> <br>
-				Share with opp: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=2 <br>"; }
+if ( $p == 1 ) {
+    echo "Link for you: <a href='bantool.php?id=$id&p=1'> LINK </a> <br>
+        Share with opp: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=2 <br>";
+}
 echo "Spect link: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=3";
 
 session_unset();	
