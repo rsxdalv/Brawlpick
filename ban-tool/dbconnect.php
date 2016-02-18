af2e32125beb73fa736c2c59de4d5c89995fca77
rsxdalv <unconfigured@null.spigotmc.org>
2016-02-18 00:37:00 +0000

    Database now uses context.

--- ban-tool/dbconnect.php
+++ ban-tool/dbconnect.php
@@ -1,7 +1,11 @@
 <?php
+include_once 'system/mysql.php';
 
 session_start();
-mysql_connect("mysql.cba.pl","zychuu97","<db password here>") or die("error with connecting to db");
-mysql_select_db("zychuu_cba_pl") or die("error with chosing db");
+
+mysql_connect($mysql_host, $mysql_user, $mysql_password) or die("error with connecting to db");
+
+mysql_select_db($mysql_database) or die("error with chosing db");
+
 mysql_query ('SET NAMES utf8');
 
--- ban-tool/system/mysql.php
+++ ban-tool/system/mysql.php
@@ -1,16 +1,22 @@
 <?php
 include_once 'context.php';
-if($_CONTEXT==="SERVER") {
+if($_CONTEXT === "SERVER") {
     $mysql_host = "mysql7.000webhost.com";
     $mysql_database = "a1202530_league";
     $mysql_user = "a1202530_lol";
     $mysql_password = "ksex69";
 }
-else if($_CONTEXT==="DEVELOPMENT") {
+else if($_CONTEXT === "DEVELOPMENT-RSX") {
     $mysql_host = "localhost";
-    $mysql_database = "Server2Go";
+    $mysql_database = "brawl-draft-pick";
     $mysql_user = "root";
-    $mysql_password = "";
+    $mysql_password = "ksex69";
+}
+else if($_CONTEXT === "Zychuu") {
+    $mysql_host = "mysql.cba.pl";
+    $mysql_database = "zychuu_cba_pl";
+    $mysql_user = "zychuu97";
+    $mysql_password = "<db password here>";
 }
 else {
     echo "Error: Unknown context!";
