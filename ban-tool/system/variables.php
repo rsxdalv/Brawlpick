562a9cad755c5b6016f6b6de97b3fa4c074eb561
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-18 00:01:15 +0000

    Import system php scripts.

---
+++ ban-tool/system/variables.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'context.php';
+// The structure leading to root
+global $_DOMAIN;
+// The amount of leagues updated per cycle
+
+if($_CONTEXT==="SERVER") {
+    $_DOMAIN = "/home/a1202530/public_html";
+}
+else if($_CONTEXT==="DEVELOPMENT") {
+    $_DOMAIN = "";
+}
+
+else {
+    echo "Error: Unknown context!";
+}
+?>
