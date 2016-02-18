2500c3e62f64835b4504cdcd157a5f31beebf698
Zychuu <zychuu97@wp.pl>
2016-02-18 19:32:33 +0100

    Tried to make it generate correct links 'for share' when it is running on diffrent hosts.

--- ban-tool/starter.php
+++ ban-tool/starter.php
@@ -10,10 +10,11 @@ $query="INSERT INTO sessions VALUES ('$id','0','0','0','0','0','0','0','1') ";
 mysql_query($query);
 
 if ( $p == 1 ) {
+	$adress=$_SERVER['HTTP_HOST']."/ban-tool";
     echo "Link for you: <a href='bantool.php?id=$id&p=1'> LINK </a> <br>
-        Share with opp: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=2 <br>";
+        Share with opp: http://$adress/bantool.php?id=$id&p=2<br> ";
 }
-echo "Spect link: http://zychuu.cba.pl/mapban/bantool.php?id=$id&p=3";
+echo "Spect link: http://$adress/bantool.php?id=$id&p=3";
 
 session_unset();	
 session_destroy();
