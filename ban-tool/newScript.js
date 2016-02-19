1fbac64ef3421b329ebe614846ff7834c36d8cb3
rsxdalv <unconfigured@null.spigotmc.org>
2016-02-19 00:23:59 +0000

    Revamped banning system. Improved style display, added sql setup archive (for future builder), added hashing script, improved javascript backend, resolved token issue.

--- ban-tool/newScript.js
+++ ban-tool/newScript.js
@@ -4,7 +4,31 @@
  * and open the template in the editor.
  */
 
+function removeVisualBan(map)
+{
+    mapElement = document.getElementById(map);
+    mapElement.className = "map";
+    mapElement.style.backgroundImage = "url('img/".concat(map).concat(".jpg')");
+    mapElement.setAttribute('onclick', 'ban('+map+')');
+}
+
 function applyVisualBan(map)
 {
-    document.getElementById(map).style.backgroundImage = "url('img/".concat(map).concat("_ban.jpg')");
+    mapElement = document.getElementById(map);
+    mapElement.className += " banned";
+    mapElement.style.backgroundImage = "url('img/".concat(map).concat("_ban.jpg')");
+    mapElement.setAttribute('onclick', '');
+}
+
+function ban(map)
+{
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(xhttp.response === "true")
+            applyVisualBan(map);
+    };
+    token = encodeURIComponent("N9KoXJgiU5KNJG/iM3H4xA==");
+    
+    xhttp.open("GET", "banRequest.php?token="+token+"&map="+map, true);
+    xhttp.send();
 }
