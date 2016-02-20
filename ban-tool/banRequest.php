de05f25e0681c2067a4af9064c7d0bfc4eb3404d
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-20 22:50:07 +0000

    Externalized map list to maps.php

--- ban-tool/banRequest.php
+++ ban-tool/banRequest.php
@@ -7,14 +7,12 @@
  */
 include "hashing.php";
 include "dbconnect.php";
+include "maps.php";
 
 $token = filter_input(INPUT_GET, "token", FILTER_SANITIZE_URL);
 $mapName = filter_input(INPUT_GET, "map", FILTER_SANITIZE_STRING);
 
 //assert($mapName !== NULL);
-
-$mapList = [keep => '1', pass => '2', fortress => '3', falls => '4', hall => '5', stadium => '6', grove => '7', 
-    1 => 'keep', 2 => 'pass', 3 => 'fortress', 4 => 'falls', 5 => 'hall', 6 => 'stadium', 7 => 'grove'];
 
 $map = $mapList[$mapName];
 
