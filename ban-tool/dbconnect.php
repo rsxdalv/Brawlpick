a0649c6e95714104cb0d4454c1731a1aa5074e72
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-20 19:51:37 +0000

    Deleted Sessions.sql

--- ban-tool/dbconnect.php
+++ ban-tool/dbconnect.php
@@ -3,7 +3,7 @@ include_once 'system/sql.php';
 
 $database_link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
 
-if (!$link) {
+if (!$database_link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
