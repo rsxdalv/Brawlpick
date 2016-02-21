3b5b906c9731386151c5d6db1a025d306d8dd3be
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-21 22:15:34 +0000

    Prepared queries implemented for status checking.

---
+++ ban-tool/maps.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$mapList = [keep => '1', pass => '2', fortress => '3', falls => '4', hall => '5', stadium => '6', grove => '7', 
+    1 => 'keep', 2 => 'pass', 3 => 'fortress', 4 => 'falls', 5 => 'hall', 6 => 'stadium', 7 => 'grove'];
