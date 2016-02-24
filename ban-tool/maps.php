60c3bcdf87ccfe5016c545874222a266ef6980e3
rsxdalv <unconfigured@null.spigotmc.org>
2016-02-24 16:08:34 +0000

    Restored support for older php versions

--- ban-tool/maps.php
+++ ban-tool/maps.php
@@ -7,5 +7,5 @@
  */
 
 
-$mapList = [keep => '1', pass => '2', fortress => '3', falls => '4', hall => '5', stadium => '6', grove => '7', 
-    1 => 'keep', 2 => 'pass', 3 => 'fortress', 4 => 'falls', 5 => 'hall', 6 => 'stadium', 7 => 'grove'];
+$mapList = array(keep => '1', pass => '2', fortress => '3', falls => '4', hall => '5', stadium => '6', grove => '7', 
+    1 => 'keep', 2 => 'pass', 3 => 'fortress', 4 => 'falls', 5 => 'hall', 6 => 'stadium', 7 => 'grove');
