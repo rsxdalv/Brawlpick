c99bfc4b9dbf79df33f79306a1aee6e0f9d464fc
rsxdalv <unconfigured@null.spigotmc.org>
2016-02-24 14:29:31 +0000

    Added new db setup configuration (works iff the new setup has been run, defaults to old one otherwise).

--- ban-tool/dbconnect.php
+++ ban-tool/dbconnect.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'system/sql.php';
+file_exists('system/db_info.php') AND include_once 'system/db_info.php';
 
 $database_link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
 
