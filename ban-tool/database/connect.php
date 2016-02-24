38eed4d5a74afd80abcb6196bc10698777e2da0a
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-24 20:52:00 +0000

    Made pages open in a new tab.

--- ban-tool/database/connect.php
+++ ban-tool/database/connect.php
@@ -1,13 +1,21 @@
 <?php
-include_once 'configuration.php';
 
-$database_link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+if( isset($OPENSHIFT_MYSQL_DB_HOST) )
+{
+    $database_link = mysqli_connect($OPENSHIFT_MYSQL_DB_HOST, $OPENSHIFT_MYSQL_DB_USER, $OPENSHIFT_MYSQL_DB_PASSWORD, $OPENSHIFT_MYSQL_DB_DATABASE);
+}
+else
+{
+    include_once 'configuration.php';
 
-if (!$database_link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+    $database_link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
+
+    if (!$database_link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
 }
 
 
