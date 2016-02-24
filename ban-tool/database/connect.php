6d72099bc51266d20d04d006b74d960df1c683be
Roberts Slisans <rsxdalv@gmail.com>
2016-02-24 21:49:03 +0000

    Improved error handling

--- ban-tool/database/connect.php
+++ ban-tool/database/connect.php
@@ -5,21 +5,16 @@ if( getenv('OPENSHIFT_PHP_IP') )
     $mysql_database = getenv(OPENSHIFT_MYSQL_DB_DATABASE);
     $mysql_user = getenv(OPENSHIFT_MYSQL_DB_USER);
     $mysql_password = getenv(OPENSHIFT_MYSQL_DB_PASSWORD);
- 
-    $database_link = mysqli_connect($OPENSHIFT_MYSQL_DB_HOST, $OPENSHIFT_MYSQL_DB_USER, $OPENSHIFT_MYSQL_DB_PASSWORD, $OPENSHIFT_MYSQL_DB_DATABASE);
 }
-else
-{
+else {
     include_once 'configuration.php';
-
-    $database_link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
-
-    if (!$database_link) {
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
 }
 
+$database_link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
 
+if (!$database_link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
