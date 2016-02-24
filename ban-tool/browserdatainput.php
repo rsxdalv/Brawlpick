b6c55b8d719fa5b35228b7b3c8ffc7511b2e63f4
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-24 09:58:13 +0000

    Upgraded input to filter_input, reformatted, idented, split query into lines, changed to mysqli from mysql, added error checking.

--- ban-tool/browserdatainput.php
+++ ban-tool/browserdatainput.php
@@ -1,16 +1,26 @@
 <?php 
 include_once 'dbconnect.php';
 
-$os=$_GET['os'];
-$browsername=$_GET['browsername'];
-$fullversion=$_GET['fullversion'];
-$ip=$_GET['ip'];
+$os = filter_input(INPUT_GET, 'os', FILTER_SANITIZE_STRING);
+$browser = filter_input(INPUT_GET, 'browsername', FILTER_SANITIZE_STRING);
+$fullversion = filter_input(INPUT_GET, 'fullversion');
+$ip = filter_input(INPUT_GET, 'ip', FILTER_SANITIZE_STRING);
 
-$winversions= array("Windows NT 5.1" => "Windows XP", "Windows NT 5.2" => "Windows XP/Sever 2003",
-					"Windows NT 6.0" => "Windows Vista", "Windows NT 6.1" => "Windows 7",
-					"Windows NT 6.2" => "Windows 8", "Windows NT 6.3" => "Windows 8.1",
-					"Windows NT 10.0" => "Windows 10");
+$winversions = array("Windows NT 5.1" => "Windows XP", "Windows NT 5.2" => "Windows XP/Sever 2003",
+                    "Windows NT 6.0" => "Windows Vista", "Windows NT 6.1" => "Windows 7",
+                    "Windows NT 6.2" => "Windows 8", "Windows NT 6.3" => "Windows 8.1",
+                    "Windows NT 10.0" => "Windows 10");
 
-$os=strtr($os, $winversions);
-					
-mysql_query("INSERT INTO browserdata VALUES ('','$os','$browsername','$fullversion','$ip') ");
+$simpleOS = strtr($os, $winversions);
+
+$query =    "INSERT INTO `browserdata`
+            VALUES ('','$simpleOS','$browsername','$fullversion','$ip') ";
+
+$result = mysqli_query($database_link, $query);
+
+if($result) {
+    echo 'true';
+}
+else {
+    echo 'false';
+}
