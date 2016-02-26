7f48a6d4ef9f99ce5c3329d4539dd67f3774f645
Roberts Slisans <rsxdalv@gmail.com>
2016-02-26 19:19:15 +0000

    Added spectator link for now. Deleted room placeholder. Added base URL placeholder.

--- enter.php
+++ enter.php
@@ -13,14 +13,15 @@ and open the template in the editor.
         <?php
         include 'hashing.php';
         $room = mt_rand(0, 0xFFFFFFFF >> 3) << 3;
-        //$room = 556;
         $token = encode_player1($room, $key, $method);
         $token2 = encode_player2($room, $key, $method);
         $token3 = encode_spectator($room, $key, $method);
+        $baseURL = "http://localhost:8080/bt/room.php?token=";
         ?>
         <a href="<?php echo 'room.php?token='.urlencode($token); ?>">Player 1</a><br />
         <a href="<?php echo 'room.php?token='.urlencode($token2); ?>">Player 2</a><br />
-        Player 2: <input type="text" value="<?php echo 'room.php?token='.urlencode($token2); ?>" id="sample1" onClick="this.setSelectionRange(0, this.value.length)" readonly><br />
-        Spectator: <input type="text" value="<?php echo 'room.php?token='.urlencode($token3); ?>" id="sample2" onClick="this.setSelectionRange(0, this.value.length)" readonly><br />
+        <a href="<?php echo 'room.php?token='.urlencode($token3); ?>">Player 2</a><br />
+        Player 2: <input type="text" value="<?php echo $baseURL.urlencode($token2); ?>" id="sample1" onClick="this.setSelectionRange(0, this.value.length)" readonly><br />
+        Spectator: <input type="text" value="<?php echo $baseURL.urlencode($token3); ?>" id="sample2" onClick="this.setSelectionRange(0, this.value.length)" readonly><br />
     </body>
 </html>
