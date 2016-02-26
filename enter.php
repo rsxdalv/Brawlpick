53bff189f9fccfc36e5cef6589bc840db2315f57
Roberts Slisans <roberts.slisans@gmail.com>
2016-02-26 19:45:54 +0000

    Fixed mt_rand returning 0 always. Removed debugging echo.

--- enter.php
+++ enter.php
@@ -16,8 +16,7 @@ and open the template in the editor.
     <body>
         <?php
         include 'hashing.php';
-        $room = mt_rand(0, (0xFFFFFFFF >> 3) ) << 3;
-        echo "Room: ".$room."<".PHP_EOL;
+        $room = mt_rand(0, 0x1FFFFFFF) << 3;
         $token = encode_player1($room, $key, $method);
         $token2 = encode_player2($room, $key, $method);
         $token3 = encode_spectator($room, $key, $method);
