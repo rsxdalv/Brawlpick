41c1be623f117f90130e512b717a8c36b04675ae
Roberts Slisans <roberts.slisans@gmail.com>
2016-03-09 13:48:28 +0000

    Introduced placeholder IV, yet it still might need a future implementation.

--- system/hashing.php
+++ system/hashing.php
@@ -6,31 +6,36 @@
  * and open the template in the editor.
  */
 
+// spectator is player 8
+define("USER_SPECTATOR", 7);
+define("USER_PLAYER1", 0);
+define("USER_PLAYER2", 1);
+        
 $key = 'A172343B239823C9';
 $method = 'aes-128-cbc';
+$iv = '1';
 
 function encode_player1($room) {
-    global $key, $method;
-    return openssl_encrypt( $room & 0xFFFFFFFE , $method, $key);
+    global $key, $method, $iv;
+    return openssl_encrypt( $room & 0xFFFFFFFE , $method, $key, 0, $iv);
 }
 
 function encode_player2($room) {
-    global $key, $method;
-    return openssl_encrypt( $room | 0x00000001 , $method, $key);
+    global $key, $method, $iv;
+    return openssl_encrypt( $room | 0x00000001 , $method, $key, 0, $iv);
 }
 
-// spectator is player 8
 function encode_spectator($room) {
-    global $key, $method;
-    return openssl_encrypt( $room | 0x00000007 , $method, $key);
+    global $key, $method, $iv;
+    return openssl_encrypt( $room | 0x00000007 , $method, $key, 0, $iv);
 }
 
 function decode_room($token) {
-    global $key, $method;
-    return openssl_decrypt($token, $method, $key) & 0xFFFFFFF8;
+    global $key, $method, $iv;
+    return openssl_decrypt($token, $method, $key, 0, $iv) & 0xFFFFFFF8;
 }
 
 function decode_player($token) {
-    global $key, $method;
-    return openssl_decrypt($token, $method, $key) & 0x00000007;
+    global $key, $method, $iv;
+    return openssl_decrypt($token, $method, $key, 0, $iv) & 0x00000007;
 }
