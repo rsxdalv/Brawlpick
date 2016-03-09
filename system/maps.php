03d47f847d887710d6631a2da2d44fb67ef416cf
Roberts Slisans <rsxdalv@gmail.com>
2016-03-09 14:10:05 +0000

    Fixed syntax for arrays.

--- system/maps.php
+++ system/maps.php
@@ -6,6 +6,8 @@
  * and open the template in the editor.
  */
 
+define("NO_MAPS_BANNED", 0);
+define("NO_UPDATES", -1);
 
-$mapList = array(keep => '1', pass => '2', fortress => '3', falls => '4', hall => '5', stadium => '6', grove => '7', 
-    1 => 'keep', 2 => 'pass', 3 => 'fortress', 4 => 'falls', 5 => 'hall', 6 => 'stadium', 7 => 'grove');
+$mapList = array('keep' => 1, 'pass' => 2, 'fortress' => 3, 'falls' => 4, 'hall' => 5, 'stadium' => 6, 'grove' => 7, 
+    '1' => 'keep', '2' => 'pass', '3' => 'fortress', '4' => 'falls', '5' => 'hall', '6' => 'stadium', '7' => 'grove');
