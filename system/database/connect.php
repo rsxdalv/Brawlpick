c40aa7a8af2db7b35bc3d68e48ba06fdc4b7d467
Roberts Slisans <rsxdalv@gmail.com>
2016-03-10 22:59:18 +0000

    Deleted obsolete code

--- system/database/connect.php
+++ system/database/connect.php
@@ -24,12 +24,3 @@ if($db->connect_errno)
 {
     echo "DB connection error. #".$db->connect_errno.": ".$db->connect_error.PHP_EOL;
 }
-//
-//$database_link = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
-//
-//if (!$database_link) {
-//    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-//    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-//    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-//    exit;
-//}
