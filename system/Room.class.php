9c5d685311fb0d582bfbc3b008e2240b92c3b0c4
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-08 20:35:26 +0100

    Applied validation method from database. Needs to replace existing usages.

--- system/Room.class.php
+++ system/Room.class.php
@@ -12,39 +12,69 @@
  * @author rober
  */
 class Room {
+
     const USER_SPECTATOR = 7;
     const USER_CONNECTED_CODE = 6; // USER_ prefix denotes data storage location
     const USER_PLAYER1 = 0;
     const USER_PLAYER2 = 1;
-    
+    const USER_BOT = 6;
+
     private static $key = 'A172343B239823C9'; // 16 bytes hexadecimal
     private static $encryption = 'aes-128-cbc'; // Simple AES method
     private static $iv = '1A2B3C4E1A2B3C4E';  // 16 bytes hexadecimal
-    
     public $id;
     public $player;
     public $token;
-    
+
     function __construct($token = NULL) {
-        if(isset($token)) {
+        if (isset($token)) {
             $this->token = $token;
-            $room_and_player = openssl_decrypt( $token , self::$encryption, self::$key, 0, self::$iv);
+            $room_and_player = openssl_decrypt($token, self::$encryption, self::$key, 0, self::$iv);
             $this->id = $room_and_player & 0xFFFFFFF8;
             $this->player = $room_and_player & 0x00000007;
         } else {
             $this->generateId();
         }
     }
-    
+
     public function getToken($player = NULL) {
         if (isset($player)) {
-            $this->token = openssl_encrypt( $this->id | $player , self::$encryption, self::$key, 0, self::$iv);
+            $this->token = openssl_encrypt($this->id | $player, self::$encryption, self::$key, 0, self::$iv);
             $this->player = $player;
         }
         return $this->token;
     }
-    
+
     private function generateId() {
         $this->id = mt_rand(0, 0xFFFFFFF) << 3;
     }
+
+    public function validate($step) {
+        if ($step > 6) {
+            return false;
+        }
+        if ($this->player === self::USER_BOT) {
+            return true;
+        }
+        switch ($step) {
+            case 1:
+            case 4:
+            case 5:
+                if ($this->player !== self::USER_PLAYER1) {
+                    return false;
+                }
+                break;
+            case 2:
+            case 3:
+            case 6:
+                if ($this->player !== self::USER_PLAYER2) {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
 }
