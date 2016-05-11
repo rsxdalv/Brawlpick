94d1b61050e4eae943afa27ee60046f2c574c99b
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 19:27:12 +0100

    Refactored static files into a public/ folder

--- views/enter.php
+++ views/enter.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Draft Pick Mode</title>
-        <link rel="stylesheet" type="text/css" href="css/entrance.css" />
+        <link rel="stylesheet" type="text/css" href="public/css/entrance.css" />
     </head>
     <body>
         <div id="wrapper">
--- views/room.php
+++ views/room.php
@@ -3,8 +3,8 @@
     <head>
         <title>Draft Pick Mode</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="css/room.css" />
-        <link rel="icon" href="img/icon.ico" />
+        <link rel="stylesheet" type="text/css" href="public/css/room.css" />
+        <link rel="icon" href="public/icon.ico" />
         <meta charset="UTF-8">
     </head>
     <body id="body" onload='init()'>
@@ -36,11 +36,11 @@
             var auth = <?php echo json_encode($token); ?>;
             var player = <?php echo json_encode($player); ?>;
         </script>
-        <script type="text/javascript" src="js/room.js"></script>
+        <script type="text/javascript" src="public/js/room.js"></script>
         <script type="text/javascript" src="https://code.createjs.com/preloadjs-0.6.2.min.js"></script>
         <script type="text/javascript">
             var queue = new createjs.LoadQueue(true);
-            queue.loadFile("img/maps_banned.jpg");
+            queue.loadFile("public/img/maps_banned.jpg");
         </script>
         </div>
     </body>
