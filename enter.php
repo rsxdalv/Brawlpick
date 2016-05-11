df940a75a068e83a9a291f45b254e2d7221ee826
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 20:15:29 +0100

    Split out controllers from routes, almost need to be in a framework

--- enter.php
+++ enter.php
@@ -1,23 +1,3 @@
 <?php 
 // CONTROLLER
-header('Cache-Control: no-store');
-
-include 'models/Room.php';
-$Room = new Room();
-
-$players['Player 1'] = urlencode($Room->getToken(Room::USER_PLAYER1));
-$players['Player 2'] = urlencode($Room->getToken(Room::USER_PLAYER2));
-$players['Spectator'] = urlencode($Room->getToken(Room::USER_SPECTATOR));
-
-$server_name = filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_URL);
-$port = filter_input(INPUT_SERVER, 'SERVER_PORT', FILTER_SANITIZE_NUMBER_INT);
-if($server_name === 'localhost') {
-    $URL = 'http://'.$server_name.':'.$port.'/bt/room.php?token=';
-} else {
-    $URL = 'http://'.$server_name.'/room.php?token=';
-}
-
-// VIEW
-include 'views/enter.php';
-?>
-
+include 'routes/room.php';
--- room.php
+++ room.php
@@ -1,16 +1,3 @@
 <?php
 // CONTROLLER
-include 'models/Room.php';
-$token = filter_input(INPUT_GET, "token", FILTER_SANITIZE_URL);
-$Room = new Room($token);
-$player = $Room->player;
-function getPlayerName( $player ) {
-    if ($player === 7) {
-        return "a spectator";
-    } else {
-        return "player " . ($player + 1);
-    }
-}
-// VIEW
-include 'views/room.php';
-?>
+include 'routes/room.php';
