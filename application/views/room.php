1dca8d8d0cbc75b8b8c0364f66dc40f69995fb31
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 23:24:50 +0100

    Temporarily Removed Bootstrap

--- application/views/room.php
+++ application/views/room.php
@@ -2,7 +2,6 @@
 <html>
     <head>
         <title>Draft Pick Mode</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="/app/public/css/room.css" />
         <link rel="icon" href="/app/public/img/icon.ico" />
         <meta charset="UTF-8">
