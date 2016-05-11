a6cecc868e3f40f91720e8a615c5c6e24ee5d134
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 18:13:21 +0100

    Improved formatting

--- enter.php
+++ enter.php
@@ -1,11 +1,14 @@
 <?php 
 // CONTROLLER
 header('Cache-Control: no-store');
+
 include 'system/Room.class.php';
 $Room = new Room();
+
 $players['Player 1'] = urlencode($Room->getToken(Room::USER_PLAYER1));
 $players['Player 2'] = urlencode($Room->getToken(Room::USER_PLAYER2));
 $players['Spectator'] = urlencode($Room->getToken(Room::USER_SPECTATOR));
+
 $server_name = filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_URL);
 $port = filter_input(INPUT_SERVER, 'SERVER_PORT', FILTER_SANITIZE_NUMBER_INT);
 if($server_name === 'localhost') {
@@ -13,6 +16,7 @@ if($server_name === 'localhost') {
 } else {
     $URL = 'http://'.$server_name.'/room.php?token=';
 }
+
 // VIEW
 ?>
 
