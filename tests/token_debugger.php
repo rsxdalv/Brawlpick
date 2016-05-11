f6d2ecb7279cbaa8fdc299567022e10d26f6615a
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 19:52:45 +0100

    Room refactoring bugfix

--- tests/token_debugger.php
+++ tests/token_debugger.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-include '../models/Room.class.php';
+include '../models/Room.php';
 
 $token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_URL);
 
