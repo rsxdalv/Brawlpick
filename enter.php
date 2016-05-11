665f09a5ba824e8bb07e9673fefe9494e6000634
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 18:12:36 +0100

    Removed legacy double assignment

--- enter.php
+++ enter.php
@@ -3,9 +3,9 @@
 header('Cache-Control: no-store');
 include 'system/Room.class.php';
 $Room = new Room();
-$players['Player 1'] = $token1 = urlencode($Room->getToken(Room::USER_PLAYER1));
-$players['Player 2'] = $token2 = urlencode($Room->getToken(Room::USER_PLAYER2));
-$players['Spectator'] = $token3 = urlencode($Room->getToken(Room::USER_SPECTATOR));
+$players['Player 1'] = urlencode($Room->getToken(Room::USER_PLAYER1));
+$players['Player 2'] = urlencode($Room->getToken(Room::USER_PLAYER2));
+$players['Spectator'] = urlencode($Room->getToken(Room::USER_SPECTATOR));
 $server_name = filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_URL);
 $port = filter_input(INPUT_SERVER, 'SERVER_PORT', FILTER_SANITIZE_NUMBER_INT);
 if($server_name === 'localhost') {
