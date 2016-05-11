9e3ed961dafc07f122abd73cf7645923f4b7fe18
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 20:04:46 +0100

    Removed empty license headers

--- models/maps.php
+++ models/maps.php
@@ -1,11 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 define("NO_MAPS_BANNED", 0);
 define("NO_UPDATES", -1);
 
--- routes/ban.php
+++ routes/ban.php
@@ -1,10 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 header('Cache-Control: no-store');
 
 require_once '../models/Database.php';
@@ -12,7 +6,6 @@ require_once '../models/Room.php';
 require_once '../models/maps.php';
 
 $token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_URL);
-// $step = filter_input(INPUT_GET, 'step', FILTER_SANITIZE_NUMBER_INT);
 $mapName = filter_input(INPUT_GET, 'map', FILTER_SANITIZE_STRING);
 
 $Room = new Room($token);
@@ -20,30 +13,6 @@ $player = $Room->player;
 $roomID = $Room->id;
 $map = $mapList[$mapName];
 assert($map !== NULL);
-
-// try{
-//     switch($step) {
-//         case 0:
-//         case 3:
-//         case 4:
-//             if($player !== Room::USER_PLAYER1) {
-//                 throw new Exception();
-//             }
-//             break;
-//         case 1:
-//         case 2:
-//         case 5:
-//             if($player !== Room::USER_PLAYER2) {
-//                 throw new Exception();
-//             }
-//             break;
-//         default:
-//                 throw new Exception();
-//     }
-// } catch (Exception $ex) {
-//     echo json_encode( array( 'success' => FALSE, 'step' => $step));
-//     exit;
-// }
 
 $database = new Database();
 echo $database->ban($roomID, $player, $map);
