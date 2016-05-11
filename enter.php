578ff6c8a91b84d652812f45f6908b6e6dbfaab1
Roberts Slisans <roberts.slisans@gmail.com>
2016-05-11 19:20:27 +0100

    Split enter.php into a view and a controller

--- enter.php
+++ enter.php
@@ -18,30 +18,6 @@ if($server_name === 'localhost') {
 }
 
 // VIEW
+include 'views/enter.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Draft Pick Mode</title>
-        <link rel="stylesheet" type="text/css" href="css/entrance.css" />
-    </head>
-    <body>
-        <div id="wrapper">
-            <?php foreach( $players as $name => $token ) {?>
-                <a class="button" href="room.php?token=<?php echo $token ?>"><?php echo $name ?></a>
-            <?php }?>
-            <a class="button" href="enter.php">New room</a>
-            <br />
-            <table>
-                <?php foreach( $players as $name => $token ) {?>
-                <tr>
-                    <td><?php echo $name ?>:</td>
-                    <td><input type="text" value="<?php echo $URL . $token; ?>" onClick="this.setSelectionRange(0, this.value.length)" readonly="" ><br /></td>
-                </tr>
-                <?php }?>
-            </table>
-        </div>
-    </body>
-</html>
